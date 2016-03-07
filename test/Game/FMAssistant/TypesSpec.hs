175c433a9228e14ce3eacfed60ef92345e75f3de
Drew Hess <src@drewhess.com>
2016-03-06 20:08:05 -0800

    Add more tests for Unpack.hs.
    Test new 'unpack' action, and test for failures.

--- test/Game/FMAssistant/TypesSpec.hs
+++ test/Game/FMAssistant/TypesSpec.hs
@@ -17,3 +17,8 @@ spec =
          let version = Version "Foo Bar"
          in
            versionToFilePath version `shouldBe` ("Foo Bar" :: FilePath)
+     describe "archiveName" $
+       it "returns the basename of an 'ArchiveFilePath'" $
+         do archiveName (ArchiveFilePath "/foo/bar/baz.rar" ) `shouldBe` "baz"
+            archiveName (ArchiveFilePath "/foo/bar/foo bar.zip" ) `shouldBe` "foo bar"
+            archiveName (ArchiveFilePath "/foo/bar/baz qux" ) `shouldBe` "baz qux"
--- test/Game/FMAssistant/UnpackSpec.hs
+++ test/Game/FMAssistant/UnpackSpec.hs
@@ -6,6 +6,7 @@ module Game.FMAssistant.UnpackSpec
        ) where
 
 import Prelude hiding (FilePath)
+import Control.Monad (void)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Managed (runManaged)
 import Data.Maybe (isNothing)
@@ -27,8 +28,17 @@ doesFileExist fp = Directory.doesFileExist (Filesystem.encodeString fp)
 zipFile :: IO ArchiveFilePath
 zipFile = ArchiveFilePath <$> Filesystem.decodeString <$> getDataFileName "data/test/test.zip"
 
+damagedZipFile :: IO ArchiveFilePath
+damagedZipFile = ArchiveFilePath <$> Filesystem.decodeString <$> getDataFileName "data/test/damaged-test.zip"
+
 rarFile :: IO ArchiveFilePath
 rarFile = ArchiveFilePath <$> Filesystem.decodeString <$> getDataFileName "data/test/test.rar"
+
+damagedRarFile :: IO ArchiveFilePath
+damagedRarFile = ArchiveFilePath <$> Filesystem.decodeString <$> getDataFileName "data/test/damaged-test.rar"
+
+anyUnpackException :: Selector UnpackException
+anyUnpackException = const True
 
 spec :: Spec
 spec =
@@ -54,6 +64,16 @@ spec =
               do zf <- liftIO zipFile
                  tmpDir <- unpackZip zf
                  liftIO $ doesFileExist (tmpDir </> "foo" </> "c.txt") `shouldReturn` True
+          it "should fail when used on a damaged ZIP file" $
+            (runManaged $
+               do zf <- liftIO damagedZipFile
+                  void $ unpackZip zf) `shouldThrow` anyUnpackException
+          it "should fail when used on a RAR file" $
+            (runManaged $
+               do rf <- liftIO rarFile
+                  void $ unpackZip rf) `shouldThrow` anyUnpackException
+          it "should fail when the file doesn't exist" $
+            (runManaged $ void $ unpackZip (ArchiveFilePath "/does/not/exist.zip")) `shouldThrow` anyUnpackException
      describe "unpackRar" $
        do it "should unpack a rar file to a temporary directory" $
             runManaged $
@@ -76,6 +96,72 @@ spec =
               do rf <- liftIO rarFile
                  tmpDir <- unpackRar rf
                  liftIO $ doesFileExist (tmpDir </> "foo" </> "c.txt") `shouldReturn` True
+          it "should fail when used on a damaged RAR file" $
+            (runManaged $
+               do rf <- liftIO damagedRarFile
+                  void $ unpackRar rf) `shouldThrow` anyUnpackException
+          it "should fail when used on a ZIP file" $
+            (runManaged $
+               do zf <- liftIO zipFile
+                  void $ unpackRar zf) `shouldThrow` anyUnpackException
+          it "should fail when the file doesn't exist" $
+            (runManaged $ void $ unpackRar (ArchiveFilePath "/does/not/exist.rar")) `shouldThrow` anyUnpackException
+     describe "unpack" $
+       do context "on RAR files" $
+            do it "should unpack the file to a temporary directory" $
+                 runManaged $
+                   do rf <- liftIO rarFile
+                      tmpDir <- unpack rf
+                      sysTmpDir <- liftIO systemTmpDir
+                      liftIO $ Filesystem.commonPrefix [sysTmpDir, tmpDir] `shouldBe` sysTmpDir
+               it "should unpack a.txt from the test file" $
+                 runManaged $
+                   do rf <- liftIO rarFile
+                      tmpDir <- unpack rf
+                      liftIO $ doesFileExist (tmpDir </> "a.txt") `shouldReturn` True
+               it "should unpack b.txt from the test file" $
+                 runManaged $
+                   do rf <- liftIO rarFile
+                      tmpDir <- unpack rf
+                      liftIO $ doesFileExist (tmpDir </> "b.txt") `shouldReturn` True
+               it "should unpack foo/c.txt from the test file" $
+                 runManaged $
+                   do rf <- liftIO rarFile
+                      tmpDir <- unpack rf
+                      liftIO $ doesFileExist (tmpDir </> "foo" </> "c.txt") `shouldReturn` True
+               it "should fail when used on a damaged file" $
+                 (runManaged $
+                    do rf <- liftIO damagedRarFile
+                       void$ unpack rf) `shouldThrow` anyUnpackException
+          context "on ZIP files" $
+            do it "should unpack the file to a temporary directory" $
+                 runManaged $
+                   do zf <- liftIO zipFile
+                      tmpDir <- unpack zf
+                      sysTmpDir <- liftIO systemTmpDir
+                      liftIO $ Filesystem.commonPrefix [sysTmpDir, tmpDir] `shouldBe` sysTmpDir
+               it "should unpack a.txt from the test file" $
+                 runManaged $
+                   do zf <- liftIO zipFile
+                      tmpDir <- unpack zf
+                      liftIO $ doesFileExist (tmpDir </> "a.txt") `shouldReturn` True
+               it "should unpack b.txt from the test file" $
+                 runManaged $
+                   do zf <- liftIO zipFile
+                      tmpDir <- unpack zf
+                      liftIO $ doesFileExist (tmpDir </> "b.txt") `shouldReturn` True
+               it "should unpack foo/c.txt from the test file" $
+                 runManaged $
+                   do zf <- liftIO zipFile
+                      tmpDir <- unpack zf
+                      liftIO $ doesFileExist (tmpDir </> "foo" </> "c.txt") `shouldReturn` True
+               it "should fail when used on a damaged file" $
+                 (runManaged $
+                    do zf <- liftIO damagedZipFile
+                       void$ unpack zf) `shouldThrow` anyUnpackException
+          context "on non-existent files" $
+            it "should fail" $
+              (runManaged $ void $ unpack (ArchiveFilePath "/does/not/exist.zip")) `shouldThrow` anyUnpackException
      describe "unpackerFor" $
        do context "on files with a \"rar\" extension" $
             do it "should unpack a rar file to a temporary directory" $
