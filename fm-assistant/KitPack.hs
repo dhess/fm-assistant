e4807f105ee5d5d3994ab29de47cc4880108a7a3
Drew Hess <src@drewhess.com>
2016-03-09 14:30:43 -0800

    Validate multiple kit packs at once.
    Note that this has uncovered an issue with using 'procStrict' to run
    subprocesses. It appears that, if you discard it, at least, the strict
    stdout returned by 'procStrict' will hang the process once the output
    reaches a certain size (probably the stdout buffer size).
    
    There's no good way in Turtle to both squash stdout & stderr and get the
    process exit code, so I'll have to work up my own solution.

--- fm-assistant/KitPack.hs
+++ fm-assistant/KitPack.hs
@@ -10,7 +10,7 @@ import Options.Applicative
 
 import Prelude hiding (FilePath)
 import Control.Monad (void)
-import qualified Data.Text as T (intercalate)
+import qualified Data.Text as T (concat)
 import qualified Data.Text.IO as T (putStrLn)
 import Filesystem.Path.CurrentOS (FilePath)
 import qualified Filesystem.Path.CurrentOS as Filesystem (decodeString)
@@ -26,7 +26,7 @@ data Command
   | Validate ValidateOptions
 
 data InstallOptions =
-  InstallOptions {_installFileName :: String}
+  InstallOptions {_installFileNames :: [String]}
 
 installCmd :: Parser Command
 installCmd = Install <$> installOptions
@@ -34,10 +34,10 @@ installCmd = Install <$> installOptions
 installOptions :: Parser InstallOptions
 installOptions =
   InstallOptions <$>
-    argument str (metavar "FILE")
+    some (argument str (metavar "FILE [FILE] ..."))
 
 data ValidateOptions =
-  ValidateOptions {_validateFileName :: String}
+  ValidateOptions {_validateFileNames :: [String]}
 
 validateCmd :: Parser Command
 validateCmd = Validate <$> validateOptions
@@ -45,27 +45,39 @@ validateCmd = Validate <$> validateOptions
 validateOptions :: Parser ValidateOptions
 validateOptions =
   ValidateOptions <$>
-    argument str (metavar "FILE")
+    some (argument str (metavar "FILE [FILE] ..."))
 
 parser :: Parser Command
 parser =
   hsubparser
-    (command "install" (info installCmd (progDesc "Install a kit pack")) <>
-     command "validate" (info validateCmd (progDesc "Validate a kit pack")))
+    (command "install" (info installCmd (progDesc "Install kit packs")) <>
+     command "validate" (info validateCmd (progDesc "Validate kit packs")))
+
+anyFailure :: ExitCode -> [ExitCode] -> ExitCode
+anyFailure failCode codes =
+  if all ((==) ExitSuccess) codes
+     then ExitSuccess
+     else failCode
 
 run :: Command -> IO ExitCode
-run (Install (InstallOptions fn)) =
-  let fp :: FilePath
-      fp = Filesystem.decodeString fn
+run (Install (InstallOptions fns)) =
+  let fps :: [FilePath]
+      fps = map Filesystem.decodeString fns
   in
-    do T.putStrLn $ fpToHumanText fp
-       return ExitSuccess
-run (Validate (ValidateOptions fn)) =
-  let fp :: FilePath
-      fp = Filesystem.decodeString fn
+    do codes <- mapM (\fp ->
+                       do T.putStrLn $ fpToHumanText fp
+                          return ExitSuccess)
+                     fps
+       return $ anyFailure (ExitFailure 1) codes
+run (Validate (ValidateOptions fns)) =
+  let fps :: [FilePath]
+      fps = map Filesystem.decodeString fns
   in
-    do exitCode <- catchesMost $
-         do void $ validateKitPack (ArchiveFilePath fp)
-            T.putStrLn $ T.intercalate " " [fpToHumanText fp, "is a valid kit pack"]
-            return ExitSuccess
-       return exitCode
+    do codes <- mapM (\fp ->
+                       do exitCode <- catchesMost $
+                            do void $ validateKitPack (ArchiveFilePath fp)
+                               T.putStrLn $ T.concat [fpToHumanText fp, ": valid kit pack"]
+                               return ExitSuccess
+                          return exitCode)
+                     fps
+       return $ anyFailure (ExitFailure 1) codes
