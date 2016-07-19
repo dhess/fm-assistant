f0de3cb0a5a10160490969472ef4fd031b8dad3c
Drew Hess <src@drewhess.com>
2016-07-19 15:44:04 -0400

    Much simpler, more robust doctests.hs.

--- test/doctests.hs
+++ test/doctests.hs
@@ -1,29 +1,6 @@
 module Main where
 
-import System.FilePath ((</>))
 import Test.DocTest
 
-addPrefix :: FilePath -> FilePath
-addPrefix fp = "src" </> "Game" </> "FMAssistant" </> fp
-
-testFiles :: [FilePath]
-testFiles =
-  map addPrefix
-      ["Magic.hs"
-      ,"Mod.hs"
-      ,"Repack.hs"
-      ,"Repack" </> "DebskisHairstyles.hs"
-      ,"Repack" </> "Faces.hs"
-      ,"Repack" </> "Internal.hs"
-      ,"Repack" </> "Kits.hs"
-      ,"Repack" </> "MetallicLogos.hs"
-      ,"Repack" </> "RealNamesFix.hs"
-      ,"Repack" </> "Skins.hs"
-      ,"Repack" </> "Unpack.hs"
-      ,"Streaming.hs"
-      ,"Types.hs"
-      ,"Util.hs"
-      ]
-
 main :: IO ()
-main = doctest testFiles
+main = doctest ["src"]
